3c0c5d2acc4604183622eb042da06b28152a86d3
Hyusein Osmanov <osmanovhyus@gmail.com>
2025-05-09 19:54:58 +0300

    initial

---
+++ lcd_initial/lcd_initial.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal.h>
+
+#define LCD_RS 12
+#define LCD_EN 11
+#define LCD_D4 5
+#define LCD_D5 4
+#define LCD_D6 3
+#define LCD_D7 2
+
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
+void setup() {
+  lcd.begin(16, 2);
+
+  lcd.print("hello, world!");
+}
+
+void loop() {
+
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+
+}
