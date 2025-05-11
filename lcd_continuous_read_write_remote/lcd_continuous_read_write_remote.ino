dc0bac8d7f0ff197ed45d584934ce1fcf01a974a
Hyusein Osmanov <osmanovhyus@gmail.com>
2025-05-11 20:11:36 +0300

    added new version of lcd: continuous writing of current selection (temperature/humidity), found bug, fixing.

---
+++ lcd_continuous_read_write_remote/lcd_continuous_read_write_remote.ino
@@ -0,0 +1,87 @@
+#include <LiquidCrystal.h>
+#include <IRremote.hpp>
+#include <dht.h>
+
+#define LCD_RS 12
+#define LCD_EN 11
+#define LCD_D4 5
+#define LCD_D5 4
+#define LCD_D6 3
+#define LCD_D7 2
+
+#define DHT_PIN 50
+
+dht DHT;
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+IRrecv IR(7);
+
+byte current_selection = 0;
+
+void setup() {
+  lcd.begin(16, 2);
+  IR.enableIRIn();
+
+  lcd.print("Waiting for command");
+}
+
+void loop() {
+  if (IR.decode()) {
+
+    byte input_from_remote = decode_hex_code(IR.decodedIRData.decodedRawData);
+    write_command(input_from_remote);
+    IR.resume();
+  }
+  if (current_selection != 0) {
+    write_command(current_selection);
+  }
+}
+
+byte decode_hex_code(unsigned long hexCode) {
+  lcd.clear();
+  lcd.print("reading..");
+  delay(500);
+  switch (hexCode) {
+    case 0xF30CFF00:
+      return 1;
+    case 0xE718FF00:
+      return 2;
+    case 0xA15EFF00:
+      return 3;
+    case 0xF708FF00:
+      return 4;
+    case 0xE31CFF00:
+      return 5;
+    case 0xA55AFF00:
+      return 6;
+    case 0xBD42FF00:
+      return 7;
+    default:
+      return 0;
+  }
+}
+
+void write_command(byte input) {
+  current_selection = input;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Input from remote was: ");
+  lcd.setCursor(0, 1);
+  if (input == 1) {
+    int chk = DHT.read11(DHT_PIN);
+    
+    lcd.print("temp: ");
+    lcd.print(DHT.temperature);
+    delay(500);
+  } else if (input == 2) {
+    
+    lcd.print("humidity: ");
+    lcd.print(DHT.humidity);
+    delay(500);
+  } 
+  if (input == 0) {
+    lcd.print("try again");
+  } else {
+    lcd.print(current_selection);
+    delay(500);
+  }
+}
