28276f9fb31c241c0a8dfe17741605a2fd9142c5
Hyusein Osmanov <osmanovhyus@gmail.com>
2025-05-15 19:33:40 +0300

    updated code, the input is being expected, while the lcd refreshes, continuously displaying dht values

--- lcd_keypad/lcd_keypad.ino
+++ lcd_keypad/lcd_keypad.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal.h>
 #include <dht.h>
 #include <Keypad.h>
+#include <string.h>
 
 #define LCD_RS 12
 #define LCD_EN 11
@@ -38,7 +39,6 @@ Keypad keypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 dht DHT;
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-
 byte current_selection = 0;
 
 void setup() {
@@ -52,75 +52,51 @@ void loop() {
 
   if (key) {
     lcd.clear();
-    lcd.print(key);
+    byte input = decode_symbol(key);
+    choose_selection(input);
   }
-  // bool isCommandWritten = false;
-  // if (IR.decode()) {
-  //   lcd.clear();
-  //   lcd.print("decoded!");
-  //   delay(500);
-  //   byte input_from_remote = decode_hex_code(IR.decodedIRData.decodedRawData);
-  //   if (input_from_remote == 0 && current_selection != 0) {
-  //     isCommandWritten = true;
-  //     write_command(current_selection);
-  //     return;
-  //   }
-  //   write_command(input_from_remote);
-  //   IR.resume();
-  // }
-  // if (current_selection != 0 && isCommandWritten == false) {
-  //   write_command(current_selection);
-  // }
-}
 
-byte decode_hex_code(unsigned long hexCode) {
-  lcd.clear();
-  lcd.print("reading..");
-  delay(500);
-  switch (hexCode) {
-    case 0xF30CFF00:
-      return 1;
-    case 0xE718FF00:
-      return 2;
-    case 0xA15EFF00:
-      return 3;
-    case 0xF708FF00:
-      return 4;
-    case 0xE31CFF00:
-      return 5;
-    case 0xA55AFF00:
-      return 6;
-    case 0xBD42FF00:
-      return 7;
-    default:
-      return 0;
+  if (current_selection != 0) {
+    print_selection();
   }
 }
 
-void write_command(byte input) {
+byte decode_symbol(char symbol) {
+  switch (symbol) {
+    case '1': return 1;
+    case '2': return 2;
+    case '3': return 3;
+    case '4': return 4;
+    case '5': return 5;
+    case '6': return 6;
+    case '7': return 7;
+    default: return 0;
+  }
+}
+
+void choose_selection(byte input) {
   current_selection = input;
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Input from remote was: ");
-  lcd.setCursor(0, 1);
-  int chk = -1;
-  if (input == 1) {
-    chk = DHT.read11(DHT_PIN);
-    
+  lcd.print("DHT input: ");
+}
+
+void print_selection() {
+  int chk = DHT.read11(DHT_PIN);
+  if (current_selection == 1) {
+    lcd.setCursor(0, 1);
+    lcd.print("                "); 
+    lcd.setCursor(0, 1);
     lcd.print("temp: ");
     lcd.print(DHT.temperature);
-    delay(500);
-  } else if (input == 2) {
-    chk = DHT.read11(DHT_PIN);
-    
+  } else if (current_selection == 2) {
+    lcd.setCursor(0, 1);
+    lcd.print("                "); 
+    lcd.setCursor(0, 1);
     lcd.print("humidity: ");
     lcd.print(DHT.humidity);
-    delay(500);
-  } 
-  if (input == 0) {
-    lcd.print("try again");
-  } else {
-    lcd.print(current_selection);
-    delay(500);
   }
+
+  delay(500);
+
 }
